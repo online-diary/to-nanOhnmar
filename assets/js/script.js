71a1fde2c650d189fe95dcf759a16b5ad8ee7aa3
mg-kh <tragonn18@gmail.com>
2020-08-23 20:45:48 +0630

    present

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,38 @@ $(document).ready(function () {
 
      $('.light').click(function () {
           $('.lighting').fadeOut(300);
+          $('.decor_container img').each(function () {
+               let topRand = function getRandomInt(min, max) {
+                    min = Math.ceil(min);
+                    max = Math.floor(max);
+                    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+               }
+               let leftRand = function getRandomInt(min, max) {
+                    min = Math.ceil(min);
+                    max = Math.floor(max);
+                    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+               }
+               $(this).css({
+                    'position': 'absolute',
+                    'top': `${topRand(0, 250)}px`,
+                    'left': `${leftRand(0, 90)}%`,
+               });
+               $(this).animate({
+                    'top': `+=${topRand(0, 700)}px`,
+               }, 10000, () => {
+                    // alert('animation is finished')
+               })
+          })
+     });
+
+     $('.up').click(function () {
+          $('.cake').remove();
           $('body').css({
                'overflow': 'auto'
           })
      })
+
+
+
+
 })
